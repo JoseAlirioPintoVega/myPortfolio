10d21365dc02949889bd5c6608dce67c2baa2ce0
Jose Pinto <alipinko123@gmail.com>
2023-03-13 19:19:14 -0500

    background de portfolio

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -4,17 +4,19 @@ import "../style/portfolio.css";
 
 const Portfolio = () => {
   return (
-    <div id="Portfolio">
-      <h3 className="title__portfolio">Portfolio</h3>
-      <p className="text__portfolio">
-        here you can see my proyects I have worked on
-      </p>
-      <div className="container__portfolio">
-        <Projects />
+    <div className="backgroundColor">
+      <div id="Portfolio">
+        <h3 className="title__portfolio">Portfolio</h3>
+        <p className="text__portfolio">
+          here you can see my proyects I have worked on
+        </p>
+        <div className="container__portfolio">
+          <Projects />
+        </div>
+        <h4 className="link-complete__portfolio">
+          <a href="#">All Proyects</a>
+        </h4>
       </div>
-      <h4 className="link-complete__portfolio">
-        <a href="#">All Proyects</a>
-      </h4>
     </div>
   );
 };
